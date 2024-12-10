22766b728c4ad960e1a26a4604c0cd642428739c
Jose Noel C. Gamba <joelgamba04@gmail.com>
2024-12-10 15:30:28 +0800

    Updated splashscreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
+  SplashScreen.preventAutoHideAsync();
+  setTimeout(SplashScreen.hideAsync, 5000);
   return (
     <>
       <Stack>
